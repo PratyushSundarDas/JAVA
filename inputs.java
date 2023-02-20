ab0928f5eda96d3a0d3bf6ed69b3d7cbffcdf583
Pratyush <pratyushsundardas@gmail.com>
2023-02-20 19:07:25 +0530

    inputs

---
+++ inputs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s = sc.nextLine();
+        System.out.println("Enter an integer:");
+        int n = sc.nextInt();
+        System.out.println("Enter a float number:");
+        float f = sc.nextFloat();
+        
+    }
+    
+}
