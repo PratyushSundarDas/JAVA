bdd451f22ad72cc3f5441f91ee1b470ff44c1295
Pratyush <pratyushsundardas@gmail.com>
2023-02-20 19:19:05 +0530

    If else example 1

---
+++ ifelse1.java
@@ -0,0 +1,17 @@
+import java.net.SocketTimeoutException;
+import java.util.*;
+class ifelse1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age:");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible for Voting");
+        }
+        else 
+    {
+            System.out.println("Not Eligible for Voting");
+    }
+    }
+    
+}
