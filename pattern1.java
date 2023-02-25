3655fa4018702ee546541abe6a6d6ebf81c21aed
Pratyush <pratyushsundardas@gmail.com>
2023-02-25 18:58:32 +0530

    Pattern

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
